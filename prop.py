5f73ada8839cbcaef49b22abfdabf42da9751b23
Igor Maksimov <st.igoriy@gmail.com>
2023-04-23 22:25:28 +0300

    23.04

--- prop.py
+++ prop.py
@@ -1,45 +1,63 @@
-from CoolProp.CoolProp import PropsSI
-import json, CoolProp.CoolProp as CP
-CP.set_config_string(CP.ALTERNATIVE_REFPROP_PATH, 'C:/Program Files (x86)/REFPROP')
+from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary
+RP = REFPROPFunctionLibrary('C:/Program Files (x86)/REFPROP')
 
+iUnits = 21
+iMass = 2
+iFlag = 0
 
 def crit(fluid):
-    T = PropsSI('TCRIT', fluid)
-    P = PropsSI('PCRIT', fluid)
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'CRIT', 'T;P', iUnits, iMass, iFlag, 0, 0, comp)
+    T = res.Output[0]
+    P = res.Output[1]
     return {'T':T,'P':P}
 
 def triple(fluid):
-    T = PropsSI('TTRIPLE', fluid)
-    P = PropsSI('PTRIPLE', fluid)
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'TRIP', 'T;P', iUnits, iMass, iFlag, 0, 0, comp)
+    T = res.Output[0]
+    P = res.Output[1]
     return {'T':T,'P':P}
 
 def R(fluid):
-    R = PropsSI('GAS_CONSTANT', fluid)/PropsSI('M', fluid)
-    return R
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'TP', 'R', iUnits, iMass, iFlag, 0, 0, comp)
+    R = res.Output[0]
+    return {'R':R}
 
 def t_p(T,P,fluid):
-    H = PropsSI('H','T', T, 'P', P, fluid)
-    A = PropsSI('A','T', T, 'P', P, fluid)
-    v = 1/PropsSI('D','T', T, 'P', P, fluid)
-    cv = PropsSI('CVMOLAR','T', T, 'P', P, fluid)/PropsSI('M', fluid)
-    cp = PropsSI('CPMOLAR','T', T, 'P', P, fluid)/PropsSI('M', fluid)
-    dPdv = -PropsSI('D','T', T, 'P', P, fluid)**2 * PropsSI('d(P)/d(D)|T','T', T, 'P', P, fluid)
-    s = PropsSI('S','T', T, 'P', P, fluid)
-    mu = PropsSI('V','T', T, 'P', P, fluid)
-    lamda = PropsSI('L','T', T, 'P', P, fluid)
-    return {'T':T, 'P':P, 'H':H, 'A':A, 'v':v, 'cv':cv,'cp':cp,'dPdv':dPdv,'s':s,'mu':mu,'lamda':lamda}
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'TP', 'T;P;H;W;V;CV;CP;DPDD;S;VIS;TCX', iUnits, iMass, iFlag, T, P, comp)
+    T = res.Output[0]
+    P = res.Output[1]
+    H = res.Output[2]
+    W = res.Output[3]
+    V = res.Output[4]
+    Cv = res.Output[5]
+    Cp = res.Output[6]
+    dPdV = -res.Output[7] / res.Output[4]**2
+    S = res.Output[8]
+    mu = res.Output[9]*res.Output[4]
+    lamda = res.Output[10]
+    return {'T':T, 'P':P, 'H':H, 'W':W, 'V':V, 'Cv':Cv,'Cp':Cp,'dPdV':dPdV,'S':S,'mu':mu,'lamda':lamda}
 
 def p_q(P,Q,fluid):
-    T = PropsSI('T', 'P', P,'Q', Q, fluid)
-    H = PropsSI('H', 'P', P,'Q', Q, fluid)
-    A = PropsSI('A', 'P', P,'Q', Q, fluid)
-    v = 1/PropsSI('D','P', P,'Q', Q, fluid)
-    
-    cv = PropsSI('CVMOLAR','P', P,'Q', Q, fluid)/PropsSI('M', fluid)
-    cp = PropsSI('CPMOLAR','P', P,'Q', Q, fluid)/PropsSI('M', fluid)
-    dPdv = -PropsSI('D','P', P,'Q', Q, fluid)**2 * PropsSI('d(P)/d(D)|T','P', P,'Q', Q, fluid)
-    s = PropsSI('S','P', P,'Q', Q, fluid)
-    mu = PropsSI('V','P', P,'Q', Q, fluid)
-    lamda = PropsSI('L','P', P,'Q', Q, fluid)
-
-    return {'T':T, 'P':P, 'H':H, 'A':A, 'v':v, 'cv':cv,'cp':cp,'dPdv':dPdv,'s':s,'mu':mu,'lamda':lamda}
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'PQ', 'T;P;H;W;V;CV;CP;DPDD;S;VIS;TCX', iUnits, iMass, iFlag, P, Q, comp)
+    T = res.Output[0]
+    P = res.Output[1]
+    H = res.Output[2]
+    W = res.Output[3]
+    V = res.Output[4]
+    Cv = res.Output[5]
+    Cp = res.Output[6]
+    dPdV = -res.Output[7] / res.Output[4]**2
+    S = res.Output[8]
+    mu = res.Output[9]*res.Output[4]
+    lamda = res.Output[10]
+    return {'T':T, 'P':P, 'H':H, 'W':W, 'V':V, 'Cv':Cv,'Cp':Cp,'dPdV':dPdV,'S':S,'mu':mu,'lamda':lamda}
